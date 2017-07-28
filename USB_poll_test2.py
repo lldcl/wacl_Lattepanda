88f05a2bb324f755ba0a962f3b7745cf6ef3fa58
lldcl <daichengliang@gmail.com>
2017-07-28 14:03:00 +0100

    Add files via upload

---
+++ USB_poll_test2.py
@@ -0,0 +1,123 @@
+import numpy as np
+import os.path
+import datetime
+import serial
+import serial.tools.list_ports
+import warnings
+import configparser
+
+import functools
+import sys
+from time import time, sleep
+import glob
+
+device_id = 1
+
+def readsave(kill):
+
+    global dataFolder, dataFile, filename, file_start_time, arduino, file_rollover, SN
+    
+    arduino_serial_number = [
+            p.serial_number
+            for p in serial.tools.list_ports.comports()
+            if 'Uno' in p.description
+        ]
+    globals()['SN'] = arduino_serial_number[device_id]
+    """Find location of the arduino"""
+    def connect_to_arduino():
+        arduino_ports = [
+            p.device
+            for p in serial.tools.list_ports.comports()
+            if 'Uno' in p.description
+        ]
+        
+        if not arduino_ports:
+            raise IOError("No Arduino found")
+        if len(arduino_ports) > 1:
+            print("Multiple Arduinos found - using the", str(globals()['SN']))
+        """Connect to the arduino"""
+        globals()['arduino'] = serial.Serial(arduino_ports[device_id],115200)
+        return(globals()['arduino'])
+        
+    """Load variables from config file"""
+    def loadconfig():
+        config = configparser.ConfigParser()
+        config.read('/home/pete/Documents/config.INI')
+        globals()['variables']=config['DataFileParams']['variables'].splitlines()
+        globals()['file_rollover'] = int(config['DataFileParams']['file_rollover'])
+        return(globals()['variables'], globals()['file_rollover'])
+        
+    """Write file header"""
+    def writeHeadertoFile(variables):
+        for v in variables:
+            dataFile.write(v)
+            dataFile.write('\t')
+        dataFile.write('\n')
+        
+    """Write dataline to file"""
+    def writeDatatoFile(datum,variables):
+        data = datum.decode().split(',')
+        """write it to file, along with a timestamp"""
+        if (len(data) == len(variables)-1):
+            """to match DAQfactory time"""
+            dataFile.write(str(datetime.datetime.now()) + ',')
+            for i in range(0,len(data)):
+                dataFile.write('\t')
+                dataFile.write(data[i].strip() + ',')
+            dataFile.write('\n')
+        else:
+            print ('Arduino data length does not match header')
+        
+    """The main loop -- data is taken here and written to file"""
+    def update():
+        if (datetime.datetime.now() < globals()['file_start_time'].replace(minute=0,second=0,microsecond=0)+datetime.timedelta(minutes=globals()['file_rollover'])):
+            """retrieve the numbers from the arduino"""
+            globals()['arduino'].write("r".encode())
+            datum = globals()['arduino'].readline()
+            writeDatatoFile(datum,variables)
+        else:
+            globals()['dataFile'].close()
+            globals()['file_start_time'] = datetime.datetime.now()
+            globals()['dataFolder'] = '/home/pete/Documents/Data/'+globals()['file_start_time'].strftime("%y_%m_%d")+'/'+ str(globals()['SN'])[-4:]+'/'
+            if not os.path.exists(dataFolder):
+                os.makedirs(dataFolder)
+            globals()['filename'] = globals()['dataFolder']+'a'+globals()['file_start_time'].strftime("%y%m%d_%H")
+            if os.path.isfile(globals()['filename']):
+                globals()['dataFile'] = open(globals()['filename'],'a',buffering=1)
+                globals()['arduino'].write("r".encode())
+                datum = globals()['arduino'].readline()
+                writeDatatoFile(datum,variables)
+            else:
+                globals()['dataFile'] = open(globals()['filename'],'a',buffering=1)
+                writeHeadertoFile(variables)
+                globals()['arduino'].write("r".encode())
+                datum = globals()['arduino'].readline()
+                writeDatatoFile(datum,variables)
+        
+    """Create datafile to save incoming data"""
+    variables, file_rollover = loadconfig()
+    file_start_time = datetime.datetime.now()
+    dataFolder = '/home/pete/Documents/Data/'+file_start_time.strftime("%y_%m_%d")+'/'+str(globals()['SN'])[-4:]+'/'
+    if not os.path.exists(dataFolder):
+        os.makedirs(dataFolder)
+    filename = dataFolder+'a'+file_start_time.strftime("%y%m%d_%H")                                  
+    if os.path.isfile(filename):
+        dataFile = open(filename,'a',buffering=1)
+    else:
+        dataFile = open(filename,'a',buffering=1)
+        writeHeadertoFile(variables)
+    globals()['arduino'] = connect_to_arduino()
+    while (kill == 'Run'):
+        readtime = time()
+        update()
+        sleep(1.-((time() - readtime) % 1.))
+    
+
+
+
+
+""" Main part of the program that sets readsave and live plotting processes going"""
+"""get rid of terminate and add ctl+c handling in subprocess"""
+if __name__ == '__main__':
+	status = 'Starting'
+	readsave('Run')
---
+++ liveplotting.py
@@ -0,0 +1,96 @@
+import pyqtgraph as pg
+from pyqtgraph.Qt import QtCore, QtGui
+from PyQt5.QtCore import QTime, QTimer
+import numpy as np
+import functools
+import sys
+from time import time, sleep
+import glob
+import re
+import datetime
+from collections import deque
+from scipy import stats
+
+class TimeAxisItem(pg.AxisItem):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def tickStrings(self, values, scale, spacing):
+        # PySide's QTime() initialiser fails miserably and dismisses args/kwargs
+        return [t.addMSecs(value).toString('HH:mm:ss') for value in values]
+
+def updateplot(data1):
+        global loc
+        try:
+                filename = sorted(glob.glob('/home/pete/Documents/Data/'+datetime.datetime.now().strftime("%y_%m_%d")+'/C2D1' + '/*'))[-1]
+        except IndexError:
+                sleep(5)
+                filename = sorted(glob.glob('/home/pete/Documents/Data/'+datetime.datetime.now().strftime("%y_%m_%d")+'/C2D1' + '/*'))[-1]
+        with open(filename) as latestfile:
+                dataline = list(latestfile)[-1].split(',')
+        print(filename)
+        if dataline[0].isalpha() == False:
+                #print(dataline[1].isalpha(), dataline[1])
+                loc+=1
+                data1[:,:-1] = data1[:,1:]
+                data1[0,-1]= np.median([np.float(dataline[1]),np.float(dataline[3]),np.float(dataline[5]),np.float(dataline[7]),np.float(dataline[9]),np.float(dataline[11]),np.float(dataline[13]),np.float(dataline[15])])
+                data1[1,-1]= np.float(dataline[3])
+                data1[2,-1]= np.float(dataline[5])
+                data1[3,-1]= np.float(dataline[7])
+                data1[4,-1]= np.float(dataline[9])
+                data1[5,-1]= np.float(dataline[11])
+                data1[6,-1]= np.float(dataline[13])
+                data1[7,-1]= np.float(dataline[15])
+                data1[8,-1]= (3655.8-np.float(dataline[71])/45.759)/100
+                data1[9,-1]= np.float(dataline[69])*0.0375-37.7
+                data1[10,-1]= ((np.float(dataline[33])*1000-225) - (np.float(dataline[35])*1000-340))/420
+                data.append({'x': t.elapsed(), 'y': data1[0,-1], 'temp_data': data1[8,-1], 'h': data1[9,-1], 'c1': data1[10,-1], 'c2': data1[11,-1]})
+                x = [item['x'] for item in data]
+                y = [item['y'] for item in data]
+                temp_data = [item['temp_data'] for item in data]
+                h = [item['h'] for item in data]
+                c1 = [item['c1'] for item in data]
+                #c2 = [item['c2'] for item in data]
+                curve1.setData(x=x, y=y)
+                curve1.setPos(-loc,0)
+                temp.setData(x=x, y=temp_data)
+                rh.setData(x=x, y=h)
+                CO1.setData(x=x, y=c1)
+                #CO2.setData(x=x, y=c2)
+
+t = QTime()
+t.start()
+data = deque(maxlen=100)
+data1 = np.zeros([12,300])
+loc = 0
+win = pg.GraphicsWindow(title='Live MOS Data')
+pg.setConfigOptions(antialias=True)
+timeaxis1 = TimeAxisItem(orientation='bottom')
+timeaxis2 = TimeAxisItem(orientation='bottom')
+timeaxis3 = TimeAxisItem(orientation='bottom')
+timeaxis4 = TimeAxisItem(orientation='bottom')
+p1 = win.addPlot(title='MOS signals', colspan=2,axisItems={'bottom': timeaxis1})
+p1.setLabel(axis='left',text='Signal (mV)')
+curve1 = p1.plot(data1[0,:],width=5)
+win.nextRow()
+p2 = win.addPlot(title='Temperature', axisItems={'bottom': timeaxis2})
+p2.setLabel(axis='left',text='Temperature (C)')
+temp = p2.plot(data1[8,:],width=5)
+p3 = win.addPlot(title='Relative Humidity', axisItems={'bottom': timeaxis3})
+p3.setLabel(axis='left',text='RH (%)')
+rh = p3.plot(data1[9,:],width=5)
+win.nextRow()
+p4 = win.addPlot(title='CO sensor signals', colspan=2, axisItems={'bottom': timeaxis4})
+p4.setLabel(axis='left',text='CO')
+CO1 = p4.plot(data1[10,:],pen=('g'),width=5)
+win.showMaximized()
+plot_update = functools.partial(updateplot,data1)
+timer = pg.QtCore.QTimer()
+timer.timeout.connect(plot_update)
+timer.start(2000)
+app = QtGui.QApplication.instance()
+sleep(5)
+
+if __name__ == '__main__':
+    if (sys.flags.interactive != 1) or not hasattr(QtCore, 'PYQT_VERSION'):
+        QtGui.QApplication.instance().exec_()
