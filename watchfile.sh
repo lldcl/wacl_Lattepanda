0f5e7e0a644206354281d19154fff5d806303ac6
lldcl <daichengliang@gmail.com>
2017-08-09 18:24:42 +0100

    Update watchfile.sh

--- watchfile.sh
+++ watchfile.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-while inotifywait -q -e modify /home/pete/Documents/Code/wacl_Lattepanda
+while (true)
 do
+	inotifywait -q -e modify /home/pete/Documents/Code/wacl_Lattepanda
 	pkill -9 -f liveplotting
 	python3 /home/pete/Documents/Code/wacl_Lattepanda/liveplotting.py
 done
